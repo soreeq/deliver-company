fad54f58643b2197442fc15154625868ed2d192a
soreeq <soreeq@gmail.com>
2023-05-03 18:41:44 +0200

    task 3 fix

--- delivery/infrastructure/src/main/java/org/example/delivery/Application.java
+++ delivery/infrastructure/src/main/java/org/example/delivery/Application.java
@@ -2,12 +2,14 @@ package org.example.delivery;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
