1d73f69757afc6dec3af6c461efc7ab64e0e9681
soreeq <soreeq@gmail.com>
2023-05-05 12:38:25 +0200

    migration created

--- delivery/infrastructure/src/main/java/org/example/delivery/Application.java
+++ delivery/infrastructure/src/main/java/org/example/delivery/Application.java
@@ -1,9 +1,11 @@
 package org.example.delivery;
 
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -12,14 +14,4 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-//    @Bean
-//    public CommandLineRunner run(UserRepository userRepository) throws Exception {
-//        return (String[] args) -> {
-//            User user1 = new User("John", "john@domain.com");
-//            User user2 = new User("Julie", "julie@domain.com");
-//            userRepository.save(user1);
-//            userRepository.save(user2);
-//            userRepository.findAll().forEach(user -> System.out.println(user));
-//        };
-//    }
 }
--- delivery/infrastructure/src/main/java/org/example/delivery/User.java
+++ delivery/infrastructure/src/main/java/org/example/delivery/User.java
@@ -1,21 +1,42 @@
 package org.example.delivery;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
-import org.springframework.data.annotation.Id;
-
+import jakarta.persistence.*;
 
 
 @Entity
-@Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private int id;
     private String name;
     private String email;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
 }
