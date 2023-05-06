f6e2227ee01c3e3415064d9ba2bb1233ca3cbb56
soreeq <soreeq@gmail.com>
2023-05-06 18:10:04 +0200

    object mapping data types

--- delivery/infrastructure/src/main/java/org/example/delivery/Application.java
+++ delivery/infrastructure/src/main/java/org/example/delivery/Application.java
@@ -12,6 +12,10 @@ import org.springframework.context.annotation.Bean;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        TemporalValues temporalValues = new TemporalValues();
+        temporalValues.setSqlDate(java.sql.Date.valueOf("2017-11-15"));
+        temporalValues.setSqlTime(java.sql.Time.valueOf("15:30:14"));
+        temporalValues.setSqlTimestamp(
+                java.sql.Timestamp.valueOf("2017-11-15 15:30:14.332"));
     }
-
 }
