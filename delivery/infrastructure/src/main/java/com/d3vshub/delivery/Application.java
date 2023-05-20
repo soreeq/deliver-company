dd666731f9af0d935f76f9b9c81e6f60c1ddc4bc
soreeq <soreeq@gmail.com>
2023-05-20 20:29:31 +0200

    added openapi documentation

--- delivery/infrastructure/src/main/java/com/d3vshub/delivery/Application.java
+++ delivery/infrastructure/src/main/java/com/d3vshub/delivery/Application.java
@@ -3,12 +3,14 @@ package com.d3vshub.delivery;
 
 import com.d3vshub.delivery.model.Courier;
 import com.d3vshub.delivery.repository.CourierRepository;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 @EnableAutoConfiguration
 public class Application {
     public static void main(String[] args) {
---
+++ delivery/infrastructure/src/main/java/com/d3vshub/delivery/SwaggerConfig.java
@@ -0,0 +1,19 @@
+/*
+package com.d3vshub.delivery;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig   {
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Delivery Management API")
+                .version("1.0.0")
+                .build();
+    }
+}
+*/
